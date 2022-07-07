3b417666695903d447d19b51ce2af1c23bed4c0d
Bootcamp Viptech <bootcamp@viptechti.int>
2022-07-07 12:15:50 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld'
 
 function App() {
+  const imagem = 'https://www.viptech.com.br/img/logo_viptech_footer.png'
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Agora vou codar em react
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='viptech'>
+      <h2>
+        Eu estou testando as maquinas da
+      </h2>
+      <div className='Colors'>
+      <img src={imagem} alt="imagem viptech" />
+        </div>
+
+      <div className='Linha-um'>
+        <h2 className='Produtos'>
+          Produtos
+        </h2>
+        <button className='EstiloBotao'>
+          adicionar produto
+        </button>
+      </div>
+
+      <HelloWorld />
     </div>
+   
   );
 }
 
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+function HelloWorld (props){
+    return(
+        <h1>
+            teste do meu primeiro componente
+        </h1>
+    )
+}
+
+export default  HelloWorld
+
