bba712d0d041ae6717ba011b8ff97c0a9b429b88
Bootcamp Viptech <bootcamp@viptechti.int>
2022-07-18 11:14:16 -0300

    refatorando o codigo

--- src/components/Products/Cart.js
+++ src/components/Products/Cart.js
@@ -4,40 +4,43 @@ import FormPropsTextFields from './AddProducts'
 import Imagem1 from './imagens/IM1.png'
 import './../../styles/CartStyles.css'
 import BuyButtonCart from './Buttons/AddBuyButton.js'
-
+import { productFake } from './FakeData.js'
+import Products from './Products'
 function Cart() {
+  console.log(productFake[0]);
   return (
+
     <div className="PaginaCart">
       <Header />
       <Link to={'/'}>Home{''}</Link>
-      <div className="TitulosPrincipaisCarrinho">
+      <div className="Introducao">
         <h1>Carrinho</h1>
-        <h1>Resumo Do Pedido</h1>
+        <h2>Resumo do pedido</h2>
       </div>
-      <div className="PaginaCarrinhoCompleta">
-        <div className="esquerda">
-          <div className="ProdutoEdescricao">
-            <img className="ImgCarrinho" src={Imagem1} alt="Imagem De Camera" />
-            <div className="Descricao">
-              <h1>Camera interna inteligente Wi-fi Full HD IM3</h1>
-              <h2>intelbras</h2>
-              <h2>Cor:Branco</h2>
-            </div>
-          </div>
-          <div className="QuantidadeEpreco">
-            <h2>Quantidade</h2>
-            <h2>Contador</h2>
-            <h2>Preço</h2>
-          </div>
-        </div>
-        <div className="Direita">
-          <h2>Subtotal(1item) R$ 300,00</h2>
-          <h2>Frete R$ 30,00</h2>
-          <h2>Valor Total R$ 330,00</h2>
-          <BuyButtonCart />
-        </div>
+      <div className="ConteudoTotal">
+        <div className='E'>
+      <div className='InformacoesProduto'>
+      <img/>
+      <div className='Description'>
+        <h2>Câmera interna inteligente Wi-Fi Full HD iM3</h2>
+        <h2>Intelbras</h2>
+        <h2>Cor:Branco</h2>
       </div>
-    </div>
+      
+      
+
+    
+
+
+      </div>
+
+        </div>
+        <div className='D'> Direita</div>
+
+      </div>
+
+      </div>
+      
   )
 }
 
